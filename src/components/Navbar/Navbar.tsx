4cb10d90f1cc03b9e66a9fee945391af53a96c20
farismecinovic <faris.mecinovic.np@gmail.com>
2021-01-08 20:22:32 +0100

    Profile made, favorite coins..

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,28 @@
 import React, { FunctionComponent } from "react";
 import { Header } from "antd/es/layout/layout";
-import { Menu } from "antd";
+import { Avatar, Badge, Menu, Image, Tooltip } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { NavLink } from "react-router-dom";
 
 const Navbar: FunctionComponent = () => {
   return (
     <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-      <Menu
-        className="menu"
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["2"]}
-      >
-        <Menu.Item key="1">Bitchain</Menu.Item>
-        <Menu.Item key="2">Home</Menu.Item>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["0"]}>
+        <Menu.Item key="1">
+          <NavLink to="/">Bitchain</NavLink>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <NavLink to="/">Home</NavLink>
+        </Menu.Item>
+        <span className="avatar-item">
+          <span>
+            <Tooltip title="Profile">
+              <NavLink to="/profile">
+                <Avatar icon={<UserOutlined />} />
+              </NavLink>
+            </Tooltip>
+          </span>
+        </span>
       </Menu>
     </Header>
   );
